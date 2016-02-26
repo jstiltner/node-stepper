157185f3bf19603053aef737271fb7ff1c8353f4
AJP <allenatmc@gmail.com>
2016-02-26 13:01:00 -0600

    add materializecss framework

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 
 app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'www')));
 
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
 app.listen(PORT, () => {
